a9ef5f2ab87bc5c5b6582ad439e0e79aff443227
Weligton <weligton0ferreira@gmail.com>
2025-11-13 19:33:40 -0300

    feat: add Inter font to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
+import { AppProvider } from '@/contexts';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const inter = Inter({
+  variable: '--font-inter',
   subsets: ['latin'],
 });
 
@@ -23,11 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='pt-BR'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang='pt-BR' className={inter.variable}>
+      <body className='antialiased'>
+        <AppProvider>{children}</AppProvider>
       </body>
     </html>
   );
