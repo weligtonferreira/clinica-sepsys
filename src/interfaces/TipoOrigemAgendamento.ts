e21c5314bba004a222728553a4a51524516cde3f
USB - IAP - Janaina Reis <janaina.reis@adventistas.org>
2025-12-09 22:21:31 -0300

    feat(interfaces): create TipoOrigemAgendamento

---
+++ src/interfaces/TipoOrigemAgendamento.ts
@@ -0,0 +1,11 @@
+export type TipoOrigemAgendamento = {
+  co_tipo: string;
+  dh_create_at: Date;
+  dh_update_at: Date;
+  ic_ativo: number;
+  no_tipo: string;
+  nu_create: number;
+  nu_ordem: number;
+  nu_tp_origem_agendamento: number;
+  nu_update: number;
+};
